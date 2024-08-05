08ae03c771a3e663c3e0fd070fe486f0f89f94d5
Kevin Almeida <kevin.almeidafsa@gmail.com>
2024-08-05 17:51:03 -0300

    update: add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
 import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 px-2">
       <h1 className="flex px-2 text-cyan-800">Teste</h1>
+      <Button>Teste</Button>
     </main>
   )
 }
